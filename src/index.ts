843934d1c2fe6316a641400f498ff178fa29e4ed
Đỗ <long@myspa.vn>
2023-07-27 10:21:08 +0700

    install helmet package

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import dotenv from 'dotenv'
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 import swaggerJsDocOptions from '~/swagger/swaggerJsDocOptions'
-import { errHandler } from "~/middlewares"
+import { errHandler } from '~/middlewares'
 import { discordBotConfig } from '~/configs'
+import helmet from 'helmet'
 
 dotenv.config()
 discordBotConfig()
 
 const PORT = process.env.POST || 4000
 const specs = swaggerJSDoc(swaggerJsDocOptions)
+app.use(helmet())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs))
 app.use(errHandler)
 
