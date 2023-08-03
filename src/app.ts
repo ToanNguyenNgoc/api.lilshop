533b18f0ae88abede940827e58a73f939c71d49e
nguyentoan06 <toan@myspa.vn>
2023-08-03 11:44:43 +0700

    [FIX] CORS 2

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,26 @@ import * as bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import initializeRouteV1 from '~/v1/routes'
 import cookieParser from 'cookie-parser'
+import cors from "cors"
+import helmet from "helmet"
 
 dotenv.config()
 class App {
   public app: express.Application
+  private origin_cors = process.env.CORS_ORIGIN?.split('|') || []
   constructor() {
     this.app = express()
     this.config()
   }
   private config(): void {
+    this.app.use(helmet())
+    this.app.use(cors({
+      origin: this.origin_cors,
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      preflightContinue: true,
+      credentials: true
+    }))
+
     this.app.use(cookieParser())
     this.app.use(bodyParser.json({ limit: '50mb' }))
     initializeRouteV1(this.app)
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import swaggerUI from 'swagger-ui-express'
 import swaggerJsDocOptions from '~/swagger/swaggerJsDocOptions'
 import { errHandler } from '~/middlewares'
 import { discordBotConfig } from '~/configs'
-import cors from "cors"
-import helmet from 'helmet'
 import hbs from "hbs"
 import path from 'path'
 import express from 'express'
@@ -17,18 +15,6 @@ discordBotConfig()
 
 const PORT = process.env.POST || 4000
 const specs = swaggerJSDoc(swaggerJsDocOptions)
-app.use(cors({
-  origin: [
-    'https://api.fashional.pro',
-    'https://fashional.pro',
-    'http://localhost:4200',
-    'http://localhost:3004'
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: true,
-  credentials: true
-}))
-app.use(helmet())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs))
 app.use(errHandler)
 //[VIEW]
