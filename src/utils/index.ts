8352ccc5e1728ab874beaafd1e519da3bd40d6a5
Nguyen Ngoc Toan <ngoctoan06011998@gmail.com>
2023-11-19 20:06:18 +0700

    ...

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,6 @@ export const currentTime = new Date()
 export const vietnamTimeZone = 'Asia/Ho_Chi_Minh';
 export const dotenvInitialize = () => dotenv.config({})
 export const momentTimeZone = moment().utcOffset('+07:00', true)
-
 export * from "./encode"
 export * from "./convert"
 export * from "./slugify"
